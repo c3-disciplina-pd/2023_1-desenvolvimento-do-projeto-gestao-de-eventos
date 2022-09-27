86cbecc210affcdddee8d13e3f9cdf732ff6f663
Davi M. G. de Almeida <66326378+davimateus1@users.noreply.github.com>
2022-09-27 08:32:28 -0300

    feat: create project with vite + chakra ui

---
+++ src/main/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Flex w="100vw" h="100vh" bg="blue">
+      <Text>Projeto criado com Vite + Chakra UI</Text>
+    </Flex>
+  );
+};
+
+export default App;
