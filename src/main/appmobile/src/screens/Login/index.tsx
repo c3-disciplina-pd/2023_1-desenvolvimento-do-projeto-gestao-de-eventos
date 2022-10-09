af950efed72fc2ef53bf9ca4ea020fd54546bf05
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2022-10-09 17:12:04 -0300

    feat: add actions login screen

--- src/main/appmobile/src/screens/Login/index.tsx
+++ src/main/appmobile/src/screens/Login/index.tsx
@@ -1,5 +1,5 @@
-import { Header } from '@components/Header';
-import * as S from './styles';
+import { Header } from "@components/Header";
+import * as S from "./styles";
 
 export function Login() {
   return (
@@ -8,10 +8,24 @@ export function Login() {
         <S.TitleTop>Events Management</S.TitleTop>
       </S.ContainerTop>
       <S.ContainerBottom>
-        <S.TitleBottom>Login</S.TitleBottom>
+        <S.TitleBottom>Entrar</S.TitleBottom>
         <S.Input placeholder="Email" />
         <S.Input placeholder="Password" secureTextEntry />
-        <S.TextForgotPassword>Forgot your password?</S.TextForgotPassword>
+        <S.ContainerForgotPassword>
+          <S.TextForgotPassword>Esqueceu sua senha?</S.TextForgotPassword>
+          <S.ActionClickHere>
+            <S.TextClickHere>Clique aqui</S.TextClickHere>
+          </S.ActionClickHere>
+        </S.ContainerForgotPassword>
+        <S.Button>
+          <S.TextButton>Entrar</S.TextButton>
+        </S.Button>
+        <S.ContainerDontHaveAccount>
+          <S.TextDontHaveAccount>Não tem uma conta?</S.TextDontHaveAccount>
+          <S.ActionRegister>
+            <S.TextRegister>Cadastre-se</S.TextRegister>
+          </S.ActionRegister>
+        </S.ContainerDontHaveAccount>
       </S.ContainerBottom>
     </S.Container>
   );
--- src/main/appmobile/src/screens/Login/styles.ts
+++ src/main/appmobile/src/screens/Login/styles.ts
@@ -52,3 +52,63 @@ export const Input = styled.TextInput`
     padding: 0 16px;
     margin-top: 32px;
 `;
+
+export const ContainerForgotPassword = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-end;
+    margin-top: 8px;
+`;
+
+export const TextForgotPassword = styled.Text`
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GREEN_700};
+`;
+
+export const ActionClickHere = styled.TouchableOpacity`
+    margin-left: 4px;
+`;
+
+export const TextClickHere = styled.Text`
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GREEN_700};
+    font-weight: bold;
+`;
+
+export const Button = styled.TouchableOpacity`
+    width: 100%;
+    height: 48px;
+    background-color: ${({ theme }) => theme.COLORS.GREEN_700};
+    border-radius: 5px;
+    margin-top: 32px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TextButton = styled.Text`
+    font-size: 16px;
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    font-weight: bold;
+`;
+
+export const ContainerDontHaveAccount = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: center;
+    margin-top: 32px;
+`;
+
+export const TextDontHaveAccount = styled.Text`
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GRAY_700};
+`;
+
+export const ActionRegister = styled.TouchableOpacity`
+    margin-left: 4px;
+`;
+
+export const TextRegister = styled.Text`
+    font-size: 14px;
+    color: ${({ theme }) => theme.COLORS.GREEN_700};
+    font-weight: bold;
+`;
