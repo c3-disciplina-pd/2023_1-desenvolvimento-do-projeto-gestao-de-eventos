204cc649df20b932bcf88bb83f7256f2cae6c8c9
Joao Vitor <outlast4321@gmail.com>
2022-11-06 12:33:48 -0300

    chore: creating login route with web tokens

--- src/main/backend/api-eventsmanagement/index.js
+++ src/main/backend/api-eventsmanagement/index.js
@@ -5,177 +5,127 @@ const conn = require("./database/database");
 const User = require("./model/User");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+require("dotenv-safe").config()
+const jwt = require('jsonwebtoken')
 conn
-  .authenticate()
-  .then(() => {
-    console.log("conexao feita");
-  })
-  .catch((msgErro) => {
-    console.log(msgErro);
-  });
+    .authenticate()
+    .then(() => {
+        console.log("conexao feita");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post('/login', (req, res) => {
+    var cpf = req.body.cpf;
+    var password = req.body.password;
+    User.findOne({
+        where: {
+            cpf: cpf,
+            password: password
+        },
+    }).then((cpf) => {
+        if (cpf) {
+            const token = jwt.sign({ cpf }, process.env.SECRET, {
+                expiresIn: 300
+            })
+            return res.json({ auth: true, token: token })
+        } else {
+            res.status(500).json({ message: 'Invalid login!' });
+        }
+
+    });
+
+
+})
+
 app.get("/", (req, res) => {
-  res.send("hello");
+    res.send("hello");
 });
 
-app.post("/users/save", async (req, res) => {
-  try {
-    var cpf = req.body.cpf;
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
-    var password = req.body.password;
-    var stringPassword = password.toString();
-    const salt = await bcrypt.genSalt();
-    const hashedPassowrd = await bcrypt.hash(stringPassword, salt);
-    console.log(salt);
-    console.log(hashedPassowrd);
-    User.create({
-      cpf: cpf,
-      firstName: firstName,
-      lastName: lastName,
-      password: hashedPassowrd,
-      email: email,
-    }).then(() => {
-      res.status(201).send();
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).send();
-  }
+app.post("/users/save", async(req, res) => {
+    try {
+        var cpf = req.body.cpf;
+        var firstName = req.body.firstName;
+        var lastName = req.body.lastName;
+        var email = req.body.email;
+        var password = req.body.password;
+        // var stringPassword = password.toString();
+        // const salt = await bcrypt.genSalt();
+        //  const hashedPassowrd = await bcrypt.hash(stringPassword, salt);
+        User.create({
+            cpf: cpf,
+            firstName: firstName,
+            lastName: lastName,
+            password: password,
+            email: email,
+        }).then(() => {
+            res.status(201).send();
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send();
+    }
 });
 
 app.get("/users/all", (req, res) => {
-  User.findAll({
-    raw: true,
-  }).then((user) => {
-    res.send(user);
-  });
+    User.findAll({
+        raw: true,
+    }).then((user) => {
+        res.send(user);
+    });
 });
 
 app.get("/users/:cpf", (req, res) => {
-  var cpf = req.params.cpf;
-  User.findOne({
-    where: {
-      cpf: cpf,
-    },
-  }).then((user) => {
-    res.send(user);
-  });
+    var cpf = req.params.cpf;
+    User.findOne({
+        where: {
+            cpf: cpf,
+        },
+    }).then((user) => {
+        res.send(user);
+    });
 });
 
 app.put("/users/update/:cpf", (req, res) => {
-  var cpf = req.params.cpf;
-  var name = req.body.name;
-  var password = req.body.password;
-  var email = req.body.email;
-  var type = req.body.type;
-  User.update(
-    {
-      name: name,
-      password: password,
-      email: email,
-      type: type,
-    },
-    {
-      where: {
-        cpf: cpf,
-      },
-    }
-  ).then(() => {
-    res.send("user " + cpf + " updated");
-  });
+    var cpf = req.params.cpf;
+    var name = req.body.name;
+    var password = req.body.password;
+    var email = req.body.email;
+    var type = req.body.type;
+    User.update({
+        name: name,
+        password: password,
+        email: email,
+        type: type,
+    }, {
+        where: {
+            cpf: cpf,
+        },
+    }).then(() => {
+        res.send("user " + cpf + " updated");
+    });
 });
 
 app.delete("/users/delete/:cpf", (req, res) => {
-  var cpf = req.params.cpf;
-  User.destroy({
-    where: {
-      cpf: cpf,
-    },
-  }).then(() => {
-    res.send("user " + cpf + " deleted");
-  });
+    var cpf = req.params.cpf;
+    User.destroy({
+        where: {
+            cpf: cpf,
+        },
+    }).then(() => {
+        res.send("user " + cpf + " deleted");
+    });
 });
 
-// ==================================================
-
-app.post("/admins/save", (req, res) => {
-  var cpf = req.body.cpf;
-  var name = req.body.name;
-  var password = req.body.password;
-  var email = req.body.email;
-
-  Admin.create({
-    cpf: cpf,
-    name: name,
-    password: password,
-    email: email,
-  }).then(() => {
-    res.send("admin " + cpf + " saved");
-  });
-});
-
-app.get("/admins/all", (req, res) => {
-  Admin.findAll({
-    raw: true,
-  }).then((user) => {
-    res.send(user);
-  });
-});
-
-app.get("/admins/:cpf", (req, res) => {
-  var cpf = req.params.cpf;
-  Admin.findOne({
-    where: {
-      cpf: cpf,
-    },
-  }).then((user) => {
-    res.send(user);
-  });
-});
-
-app.put("/admins/update/:cpf", (req, res) => {
-  var cpf = req.params.cpf;
-  var name = req.body.name;
-  var password = req.body.password;
-  var email = req.body.email;
-  Admin.update(
-    {
-      name: name,
-      password: password,
-      email: email,
-    },
-    {
-      where: {
-        cpf: cpf,
-      },
-    }
-  ).then(() => {
-    res.send("admin" + cpf + " updated");
-  });
-});
-
-app.delete("/admins/delete/:cpf", (req, res) => {
-  var cpf = req.params.cpf;
-  Admin.destroy({
-    where: {
-      cpf: cpf,
-    },
-  }).then(() => {
-    res.send("admin " + cpf + " deleted");
-  });
-});
-
-app.listen(process.env.PORT || 8080, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+app.listen(process.env.PORT || 8080, function() {
+    console.log(
+        "Express server listening on port %d in %s mode",
+        this.address().port,
+        app.settings.env
+    );
 });
