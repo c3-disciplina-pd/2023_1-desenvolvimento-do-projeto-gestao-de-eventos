721e46490fb80bd9e37c9d8c7a969b026dbdbaa4
Stevens <stevensch10@outlook.com>
2023-04-21 19:02:30 -0300

    Atualização das intruções do GUIA-BACKEND. Criação de alguns métodos no arquivo UserService.js e parando de chamar o UserRepository diretamente no userRoutes.

--- src/main/backend/api-eventsmanagement/routes/user.routes.js
+++ src/main/backend/api-eventsmanagement/routes/user.routes.js
@@ -1,6 +1,5 @@
 const {Router} = require('express');
-const UserService = require('../service/UserService')
-const UserRepository = require('../repositories/userRepository');
+const userService = require('../service/UserService')
 
 const userRoutes = new Router();
 
@@ -10,41 +9,41 @@ const userRoutes = new Router();
 
 userRoutes.post("/", async(req, res) => {
     try{
-        await UserService.register(req.body).then((user) => {
+        await userService.register(req.body).then((user) => {
             res.status(201).json(user);
         })
     }catch(e){
-        res.status(400).end()
+        res.status(400).json({message: `${e.message}`})
     }
 });
 
 userRoutes.get("/all", (req, res) => {
-    UserRepository.getAll().then((user) => {
+    userService.getAll().then((user) => {
         res.json(user);
     })
 });
 
 userRoutes.get("/:cpf", (req, res) => {
-    UserRepository.getByCpf(req.params.cpf)
+    userService.getByCpf(req.params.cpf)
     .then((user) => {
         res.json(user);
     })
 });
 
 userRoutes.patch("/update-tag/:cpf", (req, res) => {
-    UserRepository.updatePartialTag(req.params.cpf, req.body.type).then(() => {
+    userService.updatePartialTag(req.params.cpf, req.body.type).then(() => {
         res.status(200).send();
     })
 });
 
 userRoutes.put("/:cpf", (req, res) => {
-    UserRepository.update(req.params.cpf, req.body).then((user) => {
+    userService.update(req.params.cpf, req.body).then((user) => {
         res.status(200).json(user);
     })
 });
 
 userRoutes.delete("/:cpf", (req, res) => {
-    UserRepository.delete(req.params.cpf).then(() => {
+    userService.delete(req.params.cpf).then(() => {
         res.status(200).send();
     })
 });
--- src/main/backend/api-eventsmanagement/service/UserService.js
+++ src/main/backend/api-eventsmanagement/service/UserService.js
@@ -8,13 +8,35 @@ class UserService{
     //e lançar diferentes status
 
     register(body){
-        try {
-            return userRepository.add(body)
-        } catch (e) {
-            throw new Error("Falha na inserção");
+        if(userRepository.getByCpf(body.cpf) == null){
+            try {
+                return userRepository.add(body);
+            } catch (e) {
+                throw new Error("Falha na inserção");
+            }
         }
+        throw new Error("Cpf já cadastrado");
+    }
+    
+    getAll(){
+        userRepository.getAll();
     }
 
+    getByCpf(cpf){
+        userRepository.getByCpf();
+    }
+
+    updatePartialTag(cpf, type){
+        userRepository.updatePartialTag(cpf, type)
+    }
+
+    update(cpf, body){
+        userRepository.update(cpf, body)
+    }
+
+    delete(cpf){
+        userRepository.delete(cpf)
+    }
 }
 
 module.exports = new UserService();
