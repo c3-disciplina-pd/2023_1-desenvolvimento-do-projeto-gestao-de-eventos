3b8ccdb14aa87568bce1b05c8dd7eb596753f79d
Stevens <stevensch10@outlook.com>
2023-04-23 21:52:01 -0300

    Criando métodos necessários no eventService.js e atualizando o event.routes.js para não se comunicar mais com eventRepository.js.

--- src/main/backend/api-eventsmanagement/routes/event.routes.js
+++ src/main/backend/api-eventsmanagement/routes/event.routes.js
@@ -1,47 +1,38 @@
 const {Router} = require('express');
-const EventRepository = require('../repositories/eventRepository')
+const eventService = require('../service/EventService')
 
 const eventRoutes = new Router();
 
-//Ainda pode melhorar
-eventRoutes.post('/:cpf', (req, res) => {
-    var type = req.body.type;
-    EventRepository.getUserByCpfAndLastName(req.params.cpf, req.body.creator)
-    .then((cpf) => {
-        if (cpf && type == "Manager" || type == "Admin") {
-            EventRepository.add(req.body).then((event) => {
-                res.status(201).json(event);
-            })
-        } else {
-            res.status(401).json({ message: 'Invalid CPF or auth!' });
-        }
-    })
+eventRoutes.post('/:cpf', async(req, res) => {
+    try{
+        await eventService.add(req.params.cpf, req.body).then((event) => {
+            res.status(201).json(event);
+        })
+    }catch(e){
+        res.status(400).json({ message: `${e.message}` });
+    }
 })
 
 eventRoutes.get("/all", (req, res) => {
-    EventRepository.getAll().then((event) => {
+    eventService.getAll().then((event) => {
         res.send(event);
     });
 })
 
 eventRoutes.get("/:id", (req, res) => {
-    EventRepository.getById(req.params.id).then((event) => {
+    eventService.getById(req.params.id).then((event) => {
         res.send(event)
     })
 })
 
-eventRoutes.patch("/:id/:cpf", (req, res) => {
-    var type = req.body.type
-    EventRepository.getUserByCpfAndLastName(req.params.cpf, req.body.editor)
-    .then((cpf) => {
-        if (cpf && type == "Manager" || type == "Admin") {
-            EventRepository.updatePartialEmphasis(req.params.id, req.body.isEmphasis)
-            .then(() => {
-                res.status(200).send();
-            });
-        } else {
-            res.status(401).json({ message: 'Invalid CPF or auth!' });
-        }
-    })
+eventRoutes.patch("/:id/:cpf", async(req, res) => {
+    try{
+        await eventService.updatePartialEmphasis(req.params.id, req.params.cpf, req.body).then((event) => {
+            res.status(200).json(event);
+        })
+    }catch(e){
+        res.status(400).json({ message: `${e.message}` });
+    }
 })
+
 module.exports = eventRoutes;
